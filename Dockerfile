da7860097ce805aca109d6fb45490d6911dd38d8
Hammami Oussama <oussema.devstriker@gmail.com>
2018-04-19 17:12:14 +0100

    Removing global install/wait, redirect to /prod/self/fd/1

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,25 @@
 FROM node:6.13.1-stretch 
 
-RUN apt-get update && apt-get install -y curl 
+RUN apt-get update
+
+ARG user=node
+ARG group=node
+ARG userid=1337
+ARG groupid=1337
+ARG userhome=/home/node
+
+RUN usermod -u ${userid} ${user}
+RUN groupmod -g ${groupid} ${group}
 
 ADD entrypoint.sh /usr/bin/entrypoint.sh
 RUN chmod +x /usr/bin/entrypoint.sh
 
 RUN mkdir -p /website
+
+RUN chown ${user}:${group} /website
+RUN chown -R ${user}:${group} /home/node
+
+USER ${user}
 
 ENTRYPOINT ["/usr/bin/entrypoint.sh"]
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,31 +4,21 @@
 #npm install --save-dev webpack-dev-server@2.6.1
 #npm install --save-dev webpack-cli@2.0.10
 
+export PATH=${PATH}:$(pwd)/node_modules/.bin
+
 if [ -z $ENV ]; then
 	echo "No ENV set, assuming local"
 	export ENV="local"
 fi
 
 if [ -z $NODE_LOG_PATH ]; then
-	echo "No Log Path Specified, if you wish to get logs add NODE_LOG_PATH var in you environment"
-	export NODE_LOG_PATH=/dev/null
+	echo "No Log Path Specified, if you wish to get logs add NODE_LOG_PATH var to your environment"
+	export NODE_LOG_PATH=/proc/self/fd/1
 fi
 
-if [ $ENV != "prod" ]; then
-	wait -n
-	npm install --progress=false -g mocha@5.0.1 \
-		istanbul@0.4.5 \
-		apidoc@0.17.6 \
-		webpack@3.10.0 \
-		nodemon@1.17.2 >> $NODE_LOG_PATH
-fi
-
-sync
-wait -n
 npm install --progress=false >> $NODE_LOG_PATH
 
-sync
-if [ $ENV == "prod" ]; then
+if [ $ENV = "prod" ]; then
 	npm start:prod
 else
 	npm start
